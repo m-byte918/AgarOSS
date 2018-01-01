50e405dbcaac4871ac5ff6a5bfeca48512b179ab
Christian <cisr918@gmail.com>
2017-12-31 22:00:11 -0500

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,15 @@
 #include "Player.h"
 
-Player::Player() {
+Player::Player(uWS::WebSocket<uWS::SERVER> *socket) { 
+    _socket = socket;
+    _packetHandler._socket = _socket;
+}
 
+Player::~Player() { 
+
+}
+
+void Player::update() {
+    if (_mouse != _packetHandler._target)
+        _mouse = _packetHandler._target;
 }
