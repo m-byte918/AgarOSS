791da84c3909a6c729cd975d18d6aae189c6a158
Christian <cisr918@gmail.com>
2018-01-02 06:35:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "GameServer.h"
 
 // go follow https://github.com/Fan7asy he's desperate for followers
-// g++-7 -std=c++17 main.cpp GameServer.cpp Player.cpp PacketHandler.cpp ./Entity/Entity.cpp ./Modules/BinaryRW.cpp -luWS -lz -lssl
+// g++-7 -std=c++17 main.cpp GameServer.cpp Player.cpp Packet.cpp ./Entity/Entity.cpp ./Entity/EntityHandler.cpp ./Modules/BinaryRW.cpp -luWS -lz -lssl
 
 int main() {
     GameServer server = GameServer();
