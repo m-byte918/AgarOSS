e440ec45326c1e413017750135c6b95d72f2afa0
Fantasy <fantasyagario123@gmail.com>
2018-01-02 03:47:01 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "GameServer.h"
 
+// go follow https://github.com/Fan7asy he's desperate for followers
 // g++-7 -std=c++17 main.cpp GameServer.cpp Player.cpp PacketHandler.cpp ./Entity/Entity.cpp ./Modules/BinaryRW.cpp -luWS -lz -lssl
 
 int main() {
