5a2bb0a9861398dd372ee792baeca05349891a46
Christian <cisr918@gmail.com>
2018-01-02 06:21:21 -0500

    Update EntityHandler.h

--- Entity/EntityHandler.h
+++ Entity/EntityHandler.h
@@ -9,29 +9,17 @@ class Ejected;
 class MotherCell;
 class PlayerCell;
 
-static std::vector<Food*>       _foods;
-static std::vector<Virus*>      _viruses;
-static std::vector<Ejected*>    _ejecteds;
-static std::vector<MotherCell*> _mothercells;
-static std::vector<PlayerCell*> _playercells;
+namespace EntityHandler {
+    extern std::vector<Food*>       _foods;
+    extern std::vector<Virus*>      _viruses;
+    extern std::vector<Ejected*>    _ejecteds;
+    extern std::vector<MotherCell*> _mothercells;
+    extern std::vector<PlayerCell*> _playercells;
 
-static unsigned int prevNodeId = 0;
+    extern unsigned int prevNodeId;
 
-struct Color { unsigned char r, g, b; };
+    struct Color { unsigned char r, g, b; };
 
-// returns random position on the map through format { x, y }
-static Position getRandomPosition() {
-    double halfWidth = config<double>("mapWidth") / 2;
-    double halfHeight = config<double>("mapHeight") / 2;
-    return {
-        rand(-halfWidth, halfWidth), 
-        rand(-halfHeight, halfHeight)
-    };
-}
-
-static Color getRandomColor() {
-    unsigned char RGB[3] = { 255, 7, (unsigned char)rand(0, 256) };
-
-    std::shuffle(&RGB[0], &RGB[3], std::random_device{});
-    return { RGB[0], RGB[2], RGB[1] };
-}
+    extern Position getRandomPosition();
+    extern Color getRandomColor();
+};
