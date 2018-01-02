d01e9e2f2e1caff74b6d81c0176e460d1721afe5
Christian <cisr918@gmail.com>
2018-01-02 06:38:09 -0500

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,24 +1,46 @@
 #pragma once
 
 #include <vector>
-#include <memory>
 #include <uWS/uWS.h>
 
 #include "Utils.h"
-#include "PacketHandler.h"
+#include "Packet.h"
+#include "GameServer.h"
 
+enum struct PlayerState { 
+    DEAD,
+    PLAYING,
+    FREEROAM,
+    SPECTATING,
+    DISCONNECTED
+};
+
+class GameServer;
 class Player {
     private:
-        unsigned int _playerId;
-        uWS::WebSocket<uWS::SERVER> *_socket;
+        PlayerState _state = PlayerState::DEAD;
+        std::string _name = "An unnamed cell";
+        std::string _skin;
 
     public:
+        GameServer *_owner;
+        uWS::WebSocket<uWS::SERVER> *_socket;
+
+        Packet   _packet = Packet();
         Position _mouse = { 0, 0 };
+        unsigned int _playerId = ++prevPlayerId;
 
-        PacketHandler _packetHandler = PacketHandler();
+        void setState(const PlayerState &state);
+        PlayerState &getState();
 
-        Player(uWS::WebSocket<uWS::SERVER> *socket);
-        ~Player();
+        void setName(const std::string &name);
+        std::string &getName();
+
+        void setSkin(const std::string &skin);
+        std::string &getSkin();
 
         void update();
+        
+        Player(uWS::WebSocket<uWS::SERVER> *socket);
+        ~Player();
 };
