3653d1b2997bd23e75bc83f76e8163d7b86011e0
AlexH <lightweightagarioclan@gmail.com>
2018-01-02 11:48:14 +0000

    I am a strict fucker.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include "GameServer.h"
 
-// go follow https://github.com/Fan7asy he's desperate for followers
 // g++-7 -std=c++17 main.cpp GameServer.cpp Player.cpp Packet.cpp ./Entity/Entity.cpp ./Entity/EntityHandler.cpp ./Modules/BinaryRW.cpp -luWS -lz -lssl
 
 int main() {
