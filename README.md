27a4eda57ebbead61faffe2495186306befa9cfc
Megabyte918 <cisr918@gmail.com>
2018-08-02 14:49:37 -0400

    Update

--- README.md
+++
@@ -1,3 +0,0 @@
-# Rewrite
-
-This is a C++ rewrite of MultiOgar Edited, that will be released sometime in the future.
--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include "GameServer.h"
-
-// g++-7 -std=c++17 main.cpp GameServer.cpp Player.cpp Packet.cpp ./Entity/Entity.cpp ./Entity/EntityHandler.cpp ./Modules/BinaryRW.cpp -luWS -lz -lssl
-
-int main() {
-    GameServer server = GameServer();
-}
